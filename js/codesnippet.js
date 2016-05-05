9cff8c40b6d793e9fec53656fe5d481d98ff715c
Kevin Quillen <kevinquillen@gmail.com>
2016-05-05 14:46:54 -0400

    Fixes issue [#2682309]

--- js/codesnippet.js
+++ js/codesnippet.js
@@ -1,7 +1,17 @@
+/**
+ * @file
+ * Enables syntax highlighting via HighlightJS on the HTML code tag.
+ */
+
 (function ($, Drupal) {
+  'use strict';
+
   Drupal.behaviors.codesnippet = {
     attach: function () {
-      $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+      $('pre code').each(function (i, e) {
+        hljs.highlightBlock(e);
+      });
     }
-  }
+  };
+
 })(jQuery, Drupal);
